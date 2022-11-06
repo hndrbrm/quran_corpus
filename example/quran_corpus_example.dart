bcab9cb316b3aa61d07a427e3d9101255b2640eb
Eyem <eyem@Eyems-MacBook-Air.local>
2022-11-07 01:02:23 +0700

    Initial Files.

---
+++ example/quran_corpus_example.dart
@@ -0,0 +1,6 @@
+import 'package:quran_corpus/quran_corpus.dart';
+
+void main() {
+  var awesome = Awesome();
+  print('awesome: ${awesome.isAwesome}');
+}
---
+++ lib/quran_corpus.dart
@@ -0,0 +1,8 @@
+/// Support for doing something awesome.
+///
+/// More dartdocs go here.
+library quran_corpus;
+
+export 'src/quran_corpus_base.dart';
+
+// TODO: Export any libraries intended for clients of this package.
